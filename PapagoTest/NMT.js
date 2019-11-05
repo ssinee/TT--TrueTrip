0cbbc6aa7dce13b6ba6576aadbf5ac0c4b94be01
ParkSunwoo <romaakk@ajou.ac.kr>
2019-11-05 15:49:35 +0900

    파파고2

--- PapagoTest/NMT.js
+++ PapagoTest/NMT.js
@@ -6,7 +6,11 @@ var request = require('request');
 var client_id = '6EwrCUti3lul5Bue3G4g';//개발자센터에서 발급받은 Client ID
 var client_secret = 'u0fndz6mrM'; //개발자센터에서 발급받은 Client Secret
 var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
-var query = "동대문. 숭례문. 광화문.";           // 번역할 문장
+var query = "동대문\n" +
+    "숭례문\n" +
+    "광화문\n" +
+    "인생은 B와 D 사이의 C이다\n" +
+    "카카오가 카카오톡 안에서 메일을 주고받는 ‘카카오메일’ 베타서비스를 선보였다\n";      // 번역할 문장
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
