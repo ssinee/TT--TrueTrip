fc7cf3711f0b5b6f3c44f85df7e66242b3528232
이시은 <skvkffish@gmail.com>
2019-12-02 01:03:15 +0900

    please work

--- routes/showPlanner.js
+++ routes/showPlanner.js
@@ -8,39 +8,70 @@ var mongoose = require('mongoose')
 //for posts collection and planner collection
 const dbdata = mongoose.model('PostData');
 const Planner = mongoose.model('planners');
+mongoose.Promise=global.Promise
 
 
-var idcontent = [];
+var temp = []
+var idcontent=[];
 var content = [];
-var location = "";
-var category = "";
+var location="";
+var category="";
 
-showPlannersListRouter.post('/showplanner', function (req, res) {
-    res.send({'data': content, 'Iddata': idcontent, 'location': location, 'category': category})
-    // idcontent = [];
-    // content = [];
+
+showPlannersListRouter.get('/plannerlist', function (req, res) {
+    res.render('../views/plannerList.ejs', {'data':content});
+})
+// showPlannersListRouter.post('/showplanner', function (req, res) {
+//     Planner.find({'location': location}, {'id': 1, '_id': 0}, function (err, IDdata) {
+//         //사용자가 선택한 장소를 기반으로 planner의 collection안에서 filter
+//         //filter된 id바탕으로 planner의게시글 가져옴 id 통해서
+//         idcontent.push(IDdata);
+//         console.log(idcontent)
+//         for(var i=0; i<IDdata.length; i++){
+//            POSTdata= dbdata.find({'author': IDdata[i].id}, {'_id': 0, 'path': 1});
+//
+//            content.push(POSTdata)
+//             console.log(POSTdata)
+//
+//         }
+//         res.send({'data': content, 'Iddata':idcontent})
+//         idcontent=[]
+//
+//     });
+//
+//
+// })
+
+
+
+
+showPlannersListRouter.post('/showplanner', function(req, res){
+        res.send({'data':content, 'Iddata': idcontent})
+
 });
 
 
-showPlannersListRouter.post('/frommain', function (req, res) {
+showPlannersListRouter.post('/frommain', async (req, res)=> {
+    temp=[]
+    idcontent=[];
+    content=[];
     //main 화면에서 사용자가 선택한 카테고리와 위치
-    idcontent = [];
-    content = [];
     category = req.body.category;
     location = req.body.location;
-    Planner.find({'location': location}, {'id': 1, '_id': 0}, function (err, IDdata) {
+    await Planner.find({'location': location}, {'id': 1, '_id': 0}, async (err, IDdata)=> {
         //사용자가 선택한 장소를 기반으로 planner의 collection안에서 filter
         //filter된 id바탕으로 planner의게시글 가져옴 id 통해서
-        idcontent = IDdata;
+        idcontent=IDdata;
         console.log(idcontent);
-        for (var i = 0; i < IDdata.length; i++) {
+        for(var i=0; i<IDdata.length; i++){
             dbdata.find({'author': IDdata[i].id}, {'_id': 0, 'path': 1}, function (err, POSTdata) {
-                content.push(POSTdata);
-                console.log(content);
+                content.push(POSTdata)
             })
         }
+
+        res.send('1');
     });
-    res.send('1');
+
 });
 
 module.exports = showPlannersListRouter
