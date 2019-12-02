c7a1550aab0d822e4d6acd05c27453ae093371ac
이시은 <skvkffish@gmail.com>
2019-12-02 22:55:38 +0900

    category location filter added

--- routes/showPlanner.js
+++ routes/showPlanner.js
@@ -51,35 +51,77 @@ showPlannersListRouter.post('/showplanner', function (req, res) {
 });
 
 showPlannersListRouter.post('/frommain', async (req, res) => {
-    temp = []
     idcontent = [];
     content = [];
     //main 화면에서 사용자가 선택한 카테고리와 위치
     category = req.body.category;
     location = req.body.location;
-    console.log(category)
-    console.log(location)
-    await Planner.find({'location': location}, {'id': 1, '_id': 0}, async (err, IDdata) => {
-        //사용자가 선택한 장소를 기반으로 planner의 collection안에서 filter
-        //filter된 id바탕으로 planner의게시글 가져옴 id 통해서
-        idcontent = IDdata;
-        console.log(idcontent);
-        for (var i = 0; i < IDdata.length; i++) {
-            await dbdata.find({'author': IDdata[i].id, 'theme': category}, {
-                '_id': 0,
-                'path': 1
-            }, function (err, POSTdata) {
+    var plannerquery="{}"
+    var postquery="{}"
 
+    if((location=="모든지역") && (category=="모든카테고리")){
+
+        //{'location': location} planner
+        // {'author': IDdata[i].id, 'theme': category} post
+        await Planner.find({}, {'id': 1, '_id': 0}, async (err, IDdata) => {
+            idcontent = IDdata;
+            for (var i = 0; i < IDdata.length; i++) {
+                await dbdata.find({'author':IDdata[i].id}, {
+                    '_id': 0,
+                    'path': 1
+                }, function (err, POSTdata) {
                     content.push(POSTdata)
-                    console.log('in for area')
-                    console.log(POSTdata)
+                })
+            }
+            res.send('1');
+        });
+        
+    }else if(location=="모든지역"){
 
-            })
-        }
-        console.log("content")
-        console.log(content)
-        res.send('1');
-    });
+        //plannerquery null
+        // post query author theme
+        await Planner.find({}, {'id': 1, '_id': 0}, async (err, IDdata) => {
+            idcontent = IDdata;
+            for (var i = 0; i < IDdata.length; i++) {
+                await dbdata.find({'author':IDdata[i].id,'theme': category }, {
+                    '_id': 0,
+                    'path': 1
+                }, function (err, POSTdata) {
+                    content.push(POSTdata)
+                })
+            }
+            res.send('1');
+        });
+    }else if(category=="모든카테고리"){
+        console.log("category all")
+        await Planner.find({'location': location}, {'id': 1, '_id': 0}, async (err, IDdata) => {
+            idcontent = IDdata;
+            for (var i = 0; i < IDdata.length; i++) {
+                await dbdata.find({'author':IDdata[i].id }, {
+                    '_id': 0,
+                    'path': 1
+                }, function (err, POSTdata) {
+                    content.push(POSTdata)
+                })
+            }
+            res.send('1');
+        });
+    }else {
+        await Planner.find({'location': location}, {'id': 1, '_id': 0}, async (err, IDdata) => {
+            idcontent = IDdata;
+            for (var i = 0; i < IDdata.length; i++) {
+                await dbdata.find({'author':IDdata[i].id , 'theme': category}, {
+                    '_id': 0,
+                    'path': 1
+                }, function (err, POSTdata) {
+                    content.push(POSTdata)
+                })
+            }
+            res.send('1');
+        });
+
+    }
+
 
 });
 
